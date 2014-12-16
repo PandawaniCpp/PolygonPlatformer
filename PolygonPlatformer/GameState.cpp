3d0e6f81dc6327348a85caa8304481b72379118a
Marcin Jarzębski <Marcin.Jarzebski94@gmail.com>
2014-12-16 23:54:49 +0100

    Merge seems to work, i really didn't see it coming... Also, it was performed manually, Copy-Paste is still better than repository conflict solver.

--- PolygonPlatformer/GameState.cpp
+++ PolygonPlatformer/GameState.cpp
@@ -3,14 +3,14 @@
 
 
 GameState::GameState (StateStack & stack) : doSleep (true),
-                                            player (new Player),
-                                            timeStep (1.0f / 60.0f),
-                                            velocityIterations (6),
-                                            positionIterations (2){
-	
-	stateStack = &stack;
-	stateID = States::GAME;
-    
+player (new Player),
+timeStep (1.0f / 60.0f),
+velocityIterations (6),
+positionIterations (2) {
+
+    stateStack = &stack;
+    stateID = States::GAME;
+
     gravity = b2Vec2 (0.f, 100.f);
     world = new b2World (gravity);
     root.attachChild (player);
@@ -24,13 +24,13 @@ GameState::GameState (StateStack & stack) : doSleep (true),
     world->SetAllowSleeping (doSleep);
 
 
-    myBodyDef.type = b2_dynamicBody; 
-    myBodyDef.position.Set (100.f*PIXELTOMETER, 100.f*PIXELTOMETER); 
-    myBodyDef.angle = 0; 
+    myBodyDef.type = b2_dynamicBody;
+    myBodyDef.position.Set (100.f*PIXELTOMETER, 100.f*PIXELTOMETER);
+    myBodyDef.angle = 0;
 
     b2Body* dynamicBody = world->CreateBody (&myBodyDef);
 
-    boxShape.SetAsBox ((player->getTextureRect().width/2.f)*PIXELTOMETER, (player->getTextureRect().height/2)*PIXELTOMETER );
+    boxShape.SetAsBox ((player->getTextureRect ().width / 2.f)*PIXELTOMETER, (player->getTextureRect ().height / 2)*PIXELTOMETER);
 
     boxFixtureDef.shape = &boxShape;
     boxFixtureDef.density = 1;
@@ -43,7 +43,7 @@ GameState::GameState (StateStack & stack) : doSleep (true),
     /*groundBodyDef.position.Set ((1120.0f / 2.f)*PIXELTOMETER, 300.f*PIXELTOMETER);
     //groundBodyDef.type = b2_dynamicBody;
     b2Body* groundBody = world->CreateBody (&groundBodyDef);
-   
+
 
     groundBox.SetAsBox ((1120.0f/2.f)*PIXELTOMETER, (30.0f/2.f)*PIXELTOMETER);
     groundBody->CreateFixture (&groundBox, 0.0f);
@@ -59,35 +59,79 @@ GameState::GameState (StateStack & stack) : doSleep (true),
     root.attachChild (SceneNode::Ptr (new Platform ({500.f, 350.f}, {13, 2}, textures, *world)));
 
 
-	/////////////////////////////////
-	//Camera - Viewport//////////////
-	/////////////////////////////////
-	view.setCenter(player->getPosition());
+    /////////////////////////////////
+    //Camera - Viewport//////////////
+    /////////////////////////////////
+    view.setCenter (player->getPosition ());
 }
 
-void GameState::draw(sf::RenderTarget& target){
-	sf::Vector2u size = target.getSize();
-	view.setSize(size.x, size.y);
-	target.setView(view);
+void GameState::draw (sf::RenderTarget& target) {
+    sf::Vector2u size = target.getSize ();
+    view.setSize (size.x, size.y);
+    target.setView (view);
     root.draw (target);
 }
 
-bool GameState::handleEvent(const sf::Event& event) {
-	player->handleEvent(event);
+bool GameState::handleEvent (const sf::Event& event) {
+    
+        switch (event.type) {
 
-	if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::Space)
+                    createBot ();
+                break;
 
-		requestStackPush(States::PAUSE);
-	}
+            case sf::Event::KeyReleased:
+                break;
+    }
+    player->handleEvent (event);
 
-	return true;
+
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+
+        requestStackPush (States::PAUSE);
+    }
+
+
+    return true;
 }
 
 
-bool GameState::update(sf::Time dt) {
+bool GameState::update (sf::Time dt) {
     world->Step (timeStep, velocityIterations, positionIterations);
-    root.update (dt,world);
-	view.setCenter(player->getPosition());
+    root.update (dt, world);
+    view.setCenter (player->getPosition ());
 
     return true;
+}
+
+void GameState::createBot () {
+    Player::Ptr tmp (new SceneNode);
+    tmp->setTexture (textures.get (Textures::PLAYER));
+    root.attachChild (tmp);
+    tmp->setOrigin (tmp->getTextureRect ().width / 2.f, tmp->getTextureRect ().height / 2.f);
+
+
+    b2BodyDef tempBodyDef;
+    tempBodyDef.type = b2_dynamicBody;
+    tempBodyDef.position.Set ((rand () % 1120)*PIXELTOMETER, (rand () % 315)*PIXELTOMETER);
+    tempBodyDef.angle = 0;
+
+    b2Body* tempDynamicBody = world->CreateBody (&tempBodyDef);
+
+    b2FixtureDef tempBoxFixtureDef;
+
+    b2PolygonShape tempBoxShape;
+
+    tempBoxShape.SetAsBox ((tmp->getTextureRect ().width / 2.f)*PIXELTOMETER, (tmp->getTextureRect ().height / 2)*PIXELTOMETER);
+
+    tempBoxFixtureDef.shape = &boxShape;
+    tempBoxFixtureDef.density = 1;
+    tempBoxFixtureDef.friction = 1;
+
+    tempDynamicBody->CreateFixture (&tempBoxFixtureDef);
+
+    tmp->myBody = tempDynamicBody;
+
+
 }
